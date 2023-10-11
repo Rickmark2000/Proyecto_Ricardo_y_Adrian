0ad9c66e66199220653107f4ca02f3a62a42e1d4
ricardsanchez <ricardsanchez@es.gestamp.com>
2023-10-11 08:49:45 +0200

    Creacion  interface

---
+++ ISignal_Gestion.cs
@@ -0,0 +1,21 @@
+﻿using Entregable_Ricardo_y_Adrian;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Proyecto_Ricardo_y_Adrian
+{
+    public interface ISignal_Gestion
+    {
+        public void create_signal();
+        public bool add_signal(Signal signal);
+        public void add_valuesToSignal(Signal signal);
+        public void remove_signal();
+        public void search_signal(string name);
+        public void search_signal(Date date);
+
+    }
+}
---
+++ ISignal_Operations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Ricardo_y_Adrian
+{
+    public interface ISignal_Operations
+    {
+        public void max_value();
+        public void average_values();
+    }
+}
