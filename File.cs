70271766e3b40f20914ad24a90e58186dd7903f9
ricardsanchez <ricardsanchez@es.gestamp.com>
2023-10-11 08:56:20 +0200

    creacion file

---
+++ File.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Ricardo_y_Adrian
+{
+    public class File
+    {
+        private string path;
+
+        public void save_signal()
+        {
+
+        }
+
+        public List<Signals> charge_list()
+        {
+            return new List<Signals>();
+        }
+    }
+}
